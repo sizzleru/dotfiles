34e3d34260055d4ff6c6f88f5aa131d3a72a8bab
sizzleru <sizzleru@sizzleru.com>
2023-06-26 22:40:56 +1000

    fixed the execution of the setup

--- sync.sh
+++ sync.sh
@@ -3,9 +3,14 @@ echo "$(dirname "$0")" || exit 5
 git pull
 
 function setup() {
-	\ls -p src | grep -v / | xargs -I% cp src/% $HOME/%
+	ls -Ap src | grep -v / | while read -r file; do
+		if [[ -L ~/$file ]]; then
+			rm ~/$file
+		fi
+		ln -s $(pwd)/src/$file ~/$file
+	done
 }
 
 setup
 unset setup
-source ~/.bash_profile
+source ~/.bashrc
