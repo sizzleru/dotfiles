27c46cfdd28ff12972d8585f909f3121f1049bbb
sizzleru <github@sizzleru.com>
2024-01-22 12:13:49 +1100

    added shopt

---
+++ bashrc/0_shell_option.sh
@@ -0,0 +1,18 @@
+# Shopt options
+
+shopt -s cmdhist
+shopt -s direxpand
+shopt -s dotglob
+shopt -s execfail
+shopt -s expand_aliases
+shopt -s extglob
+shopt -s extquote
+shopt -s failglob
+shopt -s globstar
+shopt -s histappend
+shopt -s histreedit
+shopt -s histverify
+shopt -s lithist
+shopt -s no_empty_cmd_completion
+shopt -s nocaseglob
+shopt -s progcomp_alias
