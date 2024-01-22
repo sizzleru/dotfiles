46638695266bdaa29977536dd807056290789c83
sizzleru <github@sizzleru.com>
2024-01-22 12:27:52 +1100

    history

---
+++ bashrc/0_history.sh
@@ -0,0 +1,3 @@
+HISTIGNORE="[ \t]*:cd:ls:exit:history" # Ignore these commands on history, start the command with space or tab to ignore in the history
+HISTCONTROL=ignoreboth:erasedups
+PROMPT_COMMAND="history -n; history -w; history -c; history -r"
