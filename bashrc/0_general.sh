ec679e1281922e5bdf0d347989917b4675913fe1
sizzleru <github@sizzleru.com>
2024-01-22 20:40:48 +1100

    make bashrc rely on bash_profile

--- bashrc/0_general.sh
+++ bashrc/0_general.sh
@@ -1,3 +1,4 @@
 alias bashrc="source $HOME/.bashrc"
 alias bash_profile="source $HOME/.bash_profile"
 alias l='ls -AbBgFNopq --si --hyperlink=auto --color=auto --group-directories-first'
+alias v=$EDITOR
