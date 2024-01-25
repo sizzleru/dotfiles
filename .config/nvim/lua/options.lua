48ff5d4fc1961236b3902d9463ceeef8997888bb
sizzleru <github@sizzleru.com>
2024-01-25 18:14:56 +1100

    added nvim basic configuration

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,26 @@
+vim.opt.autoindent = true
+vim.opt.backup = true
+vim.opt.backupdir = "$XDG_STATE_HOME/nvim/backup/,/tmp/"
+vim.opt.backupskip = "/tmp/*"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.confirm = true
+vim.opt.cursorcolumn = true
+vim.opt.cursorline = true
+vim.opt.debug = "msg,throw"
+vim.opt.dictionary = "/usr/share/dict/words"
+vim.opt.diff = true
+vim.opt.expandtab = true
+vim.opt.ignorecase = true
+vim.opt.jumpoptions = "stack"
+vim.opt.list = true
+vim.opt.matchpairs = "(:),{:},[:],<:>"
+vim.opt.mouse = "iv"
+vim.opt.number = true
+vim.opt.opendevice = true
+vim.opt.shiftwidth = 4
+vim.opt.shiftround = true
+vim.opt.smartindent = true
+vim.opt.statuscolumn = "%=%{v:relnum?v:relnum:v:lnum} "
+vim.opt.statusline = "%<%f %h%m%r%=%-14.(%l,%c%V%) %P 「できるよ！」"
+vim.opt.undofile = true
+
