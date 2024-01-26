62d54a63c0f75bf4d6b4b2d84832789c80593b88
sizzleru <github@sizzleru.com>
2024-01-27 00:17:36 +1100

    small cleanup

--- bashrc/0_history.sh
+++ bashrc/0_history.sh
@@ -1,3 +1,3 @@
 HISTIGNORE="[ \t]*:cd:ls:exit:history" # Ignore these commands on history, start the command with space or tab to ignore in the history
-HISTCONTROL=ignoreboth:erasedups
-PROMPT_COMMAND="export-ps1;history -n; history -w; history -c; history -r"
+HISTCONTROL=ignoreboth:erasedups # Remove duplicate commands for less clutter
+PROMPT_COMMAND="export-ps1; history -n; history -w; history -c; history -r" # Used to update the bash prompt and update the history
