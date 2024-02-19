bf9ca543daad21b1c5de1dd60746ca2b2a594cf2
sizzleru <github@sizzleru.com>
2024-02-19 17:44:48 +1100

    fixed l function when no files in directory

--- .config/bash/bashrc/0_general.sh
+++ .config/bash/bashrc/0_general.sh
@@ -3,14 +3,18 @@ alias profile="source $HOME/.profile"
 alias v=$EDITOR
 
 function l() {
-    for f in *; do
-        file_stat=$(stat -c '%U:%G?(%A %a)?%n' "$f")
-        if [[ -L "$f" ]]; then
-            printf "%s?->?%s\n" "$file_stat" "$(readlink $f)"
-        else
-            echo "$file_stat"
-        fi
-    done | column -ts ':' -R 1 -o ':' | column -ts '?' -o ' '
+    if [[ -n "$(ls -A . 2>/dev/null)" ]]; then
+        for f in *; do
+            file_stat=$(stat -c '%U:%G?(%A %a)?%n' "$f")
+            if [[ -L "$f" ]]; then
+                printf "%s?->?%s\n" "$file_stat" "$(readlink $f)"
+            else
+                echo "$file_stat"
+            fi
+        done | column -ts ':' -R 1 -o ':' | column -ts '?' -o ' '
+    else
+        echo ''
+    fi
 }
 
 function s() {
