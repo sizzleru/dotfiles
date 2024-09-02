5683a9274ae95a5b1414ea0e8e9dca54de166273
sizzleru <github@sizzleru.com>
2024-09-02 14:13:24 +1000

    added hostname to starship prompt and various changes

---
+++ .config/starship/hostname.sh
@@ -0,0 +1,13 @@
+case "${1}" in
+    academy) echo '🏫' ;;
+    city) echo '🏙️' ;;
+    daisy) echo '🌼' ;;
+    forest) echo '🌲' ;;
+    moon) echo '🌕' ;;
+    power) echo '⚡' ;;
+    space) echo '🌌' ;;
+    stone) echo '🪨' ;;
+    tree) echo '🌳' ;;
+    workshop) echo '🛠️' ;;
+    *) echo '❓' ;;
+esac
