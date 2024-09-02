e515959cc0e5784d4b47978cfd4ccf808a07adfb
sizzleru <github@sizzleru.com>
2024-09-02 12:00:47 +1000

    starship prompt

--- .config/bash/bashrc_files/0_history.sh
+++ .config/bash/bashrc_files/0_history.sh
@@ -2,6 +2,3 @@ HISTFILE="${XDG_STATE_HOME}/bash/history"
 HISTIGNORE="[ \t]*:cd:ls:exit:history:.*ranger:lazy*" # Ignore these commands on history, start the command with space or tab to ignore in the history
 HISTSIZE=-1 # Unlimited
 HISTCONTROL='ignoreboth:erasedups' # Remove duplicate commands for less clutter
-PROMPT_COMMAND="export-ps1; history -a;" # Used to update the bash prompt and update the history
-
-#; history -n; history -w; history -c; history -r" 
---
+++ .config/bash/profile_files/0_app.sh
@@ -0,0 +1,3 @@
+# starship
+STARSHIP_CONFIG="${XDG_CONFIG_HOME}/starship/starship.toml"
+STARSHIP_CACHE="${XDG_CACHE_HOME}/starship/cache"
