e5a6c05b0f2c0fd9c2f8d381576999c4fbe66cff
sizzleru <github@sizzleru.com>
2024-09-06 15:01:35 +1000

    switched theme

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -29,7 +29,7 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "sts10/vim-pink-moon" } },
+  -- install = { colorscheme = { "sts10/vim-pink-moon" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,3 @@
 return {
-    {
-        "sts10/vim-pink-moon",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            -- load the colorscheme here
-            vim.cmd([[colorscheme pink-moon]])
-        end,
-    },
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
 }
