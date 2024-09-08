a6d5baed8bcd51efeb20e50afc8391c4ede1b891
sizzleru <github@sizzleru.com>
2024-09-09 03:48:06 +1000

    fixed polybar bug and added migration main function

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,7 +5,7 @@ polybar-msg cmd quit &>> /home/sizzleru/.cache/polybar/polybar.log
 
 # Launch a bar on each monitor
 
-MONITOR=DP-4 polybar --reload main-primary &>> /home/sizzleru/.cache/polybar/polybar.log
-MONITOR=DP-0 polybar --reload main-left &>> /home/sizzleru/.cache/polybar/polybar.log
-MONITOR=DP-2 polybar --reload main-top &>> /home/sizzleru/.cache/polybar/polybar.log
-MONITOR=HDMI-0 polybar --reload main-right &>> /home/sizzleru/.cache/polybar/polybar.log
+MONITOR=DP-4 polybar --reload main-primary &>> /home/sizzleru/.cache/polybar/polybar.log &
+MONITOR=DP-0 polybar --reload main-left &>> /home/sizzleru/.cache/polybar/polybar.log &
+MONITOR=DP-2 polybar --reload main-top &>> /home/sizzleru/.cache/polybar/polybar.log &
+MONITOR=HDMI-0 polybar --reload main-right &>> /home/sizzleru/.cache/polybar/polybar.log &
