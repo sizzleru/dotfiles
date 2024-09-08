716d3545113ebebf64bb2861ec16ccd093752749
sizzleru <github@sizzleru.com>
2024-09-08 16:32:13 +1000

    fixed a few xdg bugs, finished main migrations

--- .config/bash/profile_files/0_app.sh
+++ .config/bash/profile_files/0_app.sh
@@ -1,3 +1,8 @@
-# starship
-STARSHIP_CONFIG="${XDG_CONFIG_HOME}/starship/starship.toml"
-STARSHIP_CACHE="${XDG_CACHE_HOME}/starship/cache"
+STARSHIP_CONFIG='/home/sizzleru/.config/starship/starship.toml'
+STARSHIP_CACHE='/home/sizzleru/.cache/starship/cache'
+PYTHON_HISTORY='/home/sizzleru/.local/state/python/history'
+CUDA_CACHE_PATH='/home/sizzleru/.cache/nv'
+PYTHON_HISTORY='/home/sizzleru/.local/state/python/history'
+alias nvidia-settings='nvidia-settings --config=/home/sizzleru/.config/nvidia/settings'
+HISTFILE='/home/sizzleru/.local/state/bash/history'
+XINITRC='/home/sizzleru/.config/X11/xinitrc'
