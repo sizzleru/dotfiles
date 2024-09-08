3941dae51ba9e900a658fba67a228479bfa31473
sizzleru <github@sizzleru.com>
2024-09-08 23:19:28 +1000

    fixed monitors not being properly adjusted before applications start launching

--- .config/bash/bashrc_files/0_history.sh
+++ .config/bash/bashrc_files/0_history.sh
@@ -2,4 +2,4 @@ HISTFILE="${XDG_STATE_HOME:-${HOME}/.local/state}/bash/history"
 HISTIGNORE="[ \t]*" # Ignore these commands on history, start the command with space or tab to ignore in the history
 HISTTIMEFORMAT="%Y/%m/%d %H:%M:%S => "
 HISTSIZE=-1 # Unlimited
-HISTCONTROL='ignoredups:erasedups:ignorespace:ignore:both' # Remove duplicate commands for less clutter
+HISTCONTROL='ignoredups:erasedups' # Remove duplicate commands for less clutter
