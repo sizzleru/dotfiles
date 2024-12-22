f1fd010155bdabf8c44ecb57c0e3573010f1a321
sizzleru <github@sizzleru.com>
2024-12-23 09:24:55 +1100

    Revert "will differ across systems"
    This reverts commit 1134aa0386e83ca8c564f6227c7da1659292cb92.

---
+++ .config/bash/profile_files/0_app.sh
@@ -0,0 +1,27 @@
+STARSHIP_CONFIG='/home/sizzleru/.config/starship/starship.toml'
+STARSHIP_CACHE='/home/sizzleru/.cache/starship/cache'
+PYTHON_HISTORY='/home/sizzleru/.local/state/python/history'
+CUDA_CACHE_PATH='/home/sizzleru/.cache/nv'
+PYTHON_HISTORY='/home/sizzleru/.local/state/python/history'
+alias nvidia-settings='nvidia-settings --config=/home/sizzleru/.config/nvidia/settings'
+HISTFILE='/home/sizzleru/.local/state/bash/history'
+XINITRC='/home/sizzleru/.config/X11/xinitrc'
+INPUTRC='/home/sizzleru/.config/readline/inputrc'
+GTK_IM_MODULE='ibus'
+QT_IM_MODULE='ibus'
+XMODIFIERS=@im='ibus'
+FZF_DEFAULT_OPTS=" \
+--color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 \
+--color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
+--color=marker:#babbf1,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284 \
+--color=selected-bg:#51576d \
+--multi"
+#XAUTHORITY='/run/user/1000/xorg-xauth/Xauthority'
+WGETRC='/home/sizzleru/.config/wget/wgetrc'
+alias wget='wget --hsts=file=/home/sizzleru/.cache/wget/wget-hsts'
+alias yarn='yarn --use-yarnrc /home/sizzleru/.config/yarn/config'
+GTK2_RC_FILES='/home/sizzleru/.config/gtk-2.0/settings.ini'
+TERMINFO='/home/sizzleru/.local/share/terminfo'
+TERMINFO_DIRS=/home/sizzleru/.local/share/terminfo:/usr/share/terminfo
+GOPATH='/home/sizzleru/.local/share/go'
+GOMODCACHE=/home/sizzleru/.cache/go/mod
