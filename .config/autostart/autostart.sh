b7d3d1b987afc5a6b92d13398136e57a2c94b043
sizzleru <github@sizzleru.com>
2025-01-01 15:19:45 +1100

    moved sshfs mounts to /etc/fstab

--- .config/autostart/autostart.sh
+++ .config/autostart/autostart.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-dex "${XDG_CONFIG_HOME:-${HOME}/.config}/autostart/xrandr.desktop" >> "${XDG_CACHE_HOME:-${HOME}/.cache}/dex/dex.log"
+: ${XDG_CONFIG_HOME:=${HOME}/.config}
+: ${XDG_CACHE_HOME:=${HOME}/.cache}
+
+dex "${XDG_CONFIG_HOME}/autostart/xrandr.desktop" >> "${XDG_CACHE_HOME}/dex/dex.log" && \
+dex "${XDG_CONFIG_HOME}/autostart/sshfs.desktop" >> "${XDG_CACHE_HOME}/dex/dex.log" && \
 dex -a >> "${XDG_CACHE_HOME:-${HOME}/.cache}/dex/dex.log"
--- .config/bash/bashrc_files/0_general.sh
+++ .config/bash/bashrc_files/0_general.sh
@@ -60,6 +60,13 @@ function y() {
     rm -f -- "$tmp"
 }
 
+function sshfs_mount() {
+	if [[ ! $(tailscale status | grep 'moon.*offline' | wc -l) -eq 1 ]]; then
+		cat /home/sizzleru/.config/tree/sshfs_mounts.txt | awk '{ print $3 }' | xargs -n 1 mkdir -pv
+		cat /home/sizzleru/.config/tree/sshfs_mounts.txt | awk '{ print $1":"$2" "$3 }' | xargs -n 2 sshfs
+	fi
+}
+
 function sshfs_umount() {
 	cat /home/sizzleru/.config/tree/sshfs_mounts.txt | awk '{ print $3 }' | xargs -n 1 sudo umount
 }
