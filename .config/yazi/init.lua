abb11b49ddea01f93dff0505ea59f2e1db5a4994
sizzleru <github@sizzleru.com>
2025-01-10 08:30:25 +1100

    yazi bookmarks

---
+++ .config/yazi/init.lua
@@ -0,0 +1,16 @@
+-- ~/.config/yazi/init.lua
+require("bookmarks"):setup({
+	last_directory = { enable = true, persist = true },
+	persist = "all",
+	desc_format = "parent",
+	file_pick_mode = "parent",
+	notify = {
+		enable = true,
+		timeout = 1,
+		message = {
+			new = "New bookmark '<key>' -> '<folder>'",
+			delete = "Deleted bookmark in '<key>'",
+			delete_all = "Deleted all bookmarks",
+		},
+	},
+})
