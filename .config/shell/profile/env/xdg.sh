cb9f8469f8a75b8ce901322ff66d94e85bf61d90
sizzleru <github@sizzleru.com>
2026-04-22 11:38:40 +1000

    [glue]: added back some env for xdg applications

--- .config/shell/profile/env/xdg.sh
+++ .config/shell/profile/env/xdg.sh
@@ -1,2 +1,13 @@
+# Terminal
+export TERMINAL='kitty'
+
 # Editor
+export EDITOR='vim'
 export VISUAL="${EDITOR}"
+
+# System
+export CLIPBOARD='wl-clipboard'
+
+# General
+export FILE_MANAGER='thunar'
+export BROWSER='vivaldi'
