416bb0c7f668dc4f8dd80384ef430208bcf0f3ea
sizzleru <github@sizzleru.com>
2026-04-22 11:29:35 +1000

    Split up applications

--- .config/shell/profile/env/main.sh
+++ .config/shell/profile/env/main.sh
@@ -1,4 +1,4 @@
-for file in path applications; do
+for file in path xdg; do
 	if [ -f "${HOME}/.config/shell/profile/env/${file}.sh" ]; then
 		source "${HOME}/.config/shell/profile/env/${file}.sh"
 	fi
