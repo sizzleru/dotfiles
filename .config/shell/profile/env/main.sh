743481505a8ce30e453645635fc71802e80fe480
sizzleru <github@sizzleru.com>
2026-05-07 22:42:33 +1000

    [glue]: add relevant host and user details

--- .config/shell/profile/env/main.sh
+++ .config/shell/profile/env/main.sh
@@ -1,4 +1,4 @@
-for file in path xdg; do
+for file in host path xdg; do
 	if [ -f "${HOME}/.config/shell/profile/env/${file}.sh" ]; then
 		source "${HOME}/.config/shell/profile/env/${file}.sh"
 	fi
