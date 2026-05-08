95cb5232b5f1af1ff22945505aa89ac9a46d0ed0
sizzleru <github@sizzleru.com>
2026-05-08 11:19:47 +1000

    [glue]: POSIX-ify my other glues lol

--- .config/shell/profile/shell/main.sh
+++ .config/shell/profile/shell/main.sh
@@ -1,5 +1,5 @@
 for file in bashrc; do
 	if [ -f "${HOME}/.config/shell/profile/shell/${file}.sh" ]; then
-		source "${HOME}/.config/shell/profile/shell/${file}.sh"
+		. "${HOME}/.config/shell/profile/shell/${file}.sh"
 	fi
 done
