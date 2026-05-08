3b633afdde5303b1cf4dc00f793ec1ec8a6a6ed9
sizzleru <github@sizzleru.com>
2026-05-08 11:17:46 +1000

    POSIX-ify some bloody more

--- .config/shell/bashrc/aliases/main.sh
+++ .config/shell/bashrc/aliases/main.sh
@@ -1,5 +1,5 @@
 for file in editor fetch git trashcli; do
 	if [ -f "${HOME}/.config/shell/bashrc/aliases/${file}.sh" ]; then
-		source "${HOME}/.config/shell/bashrc/aliases/${file}.sh"
+		. "${HOME}/.config/shell/bashrc/aliases/${file}.sh"
 	fi
 done
--- .config/shell/bashrc/shell/main.sh
+++ .config/shell/bashrc/shell/main.sh
@@ -1,5 +1,5 @@
 for file in interactive bash history display_manager; do
 	if [ -f "${HOME}/.config/shell/bashrc/shell/${file}.sh" ]; then
-		source "${HOME}/.config/shell/bashrc/shell/${file}.sh"
+		. "${HOME}/.config/shell/bashrc/shell/${file}.sh"
 	fi
 done
