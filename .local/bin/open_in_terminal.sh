1d6fe7862b8ab8a92bb5dca6606c9f4bdc68bbf1
sizzleru <github@sizzleru.com>
2026-05-19 13:16:51 +1000

    Added a script to allow opening a directory in terminal and supports sftp mounted dirs

---
+++ .local/bin/open_in_terminal.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+usage() {
+	printf 'Usage: %s [-d|--directory directory] [-h|--help]\n' "${0}" >&2
+	exit 1
+}
+
+while [ "${#}" -gt 0 ]; do
+	case "${1}" in
+		-d|--directory)
+			if [ -z "${2}" ]; then
+				printf -- '-|--file requires an argument\n' >&2
+				usage
+			fi
+
+			directory="${2}"
+			shift 2
+			;;
+		-h|--help)
+			usage
+			;;
+		-*)
+			printf 'Unknown flag: %s\n' "${1}" >&2
+			usage
+			;;
+		*)
+			break
+			;;
+	esac
+done
+
+# Set default directory to ${HOME} if not available
+: "${directory:="${HOME}"}"
+
+# check if sftp mounted
+case "${directory}" in
+	"/run/user/$( id -u )/gvfs/sftp:host="*",user=$( id -un )"*)
+		SSH_USER="$( echo "${directory}" | grep -oE 'user=[^/]+/' | sed -E -- 's-(^user=|/$)--g' )"
+		SSH_HOST="$( echo "${directory}" | grep -oE 'host=[^,]+,' | sed -E -- 's-(^host=|,$)--g' )"
+		SSH_DIRECTORY="$( echo "${directory}" | sed -E -- "s/.*user=${SSH_USER}//" )"
+
+		if command -v kitty >/dev/null; then
+			if command -v kitten >/dev/null; then
+				kitty kitty +kitten ssh -t "${SSH_USER}@${SSH_HOST}" "cd ${SSH_DIRECTORY} && exec \${SHELL} -l"
+			else
+				kitty ssh -t "${SSH_USER}@${SSH_HOST}" "cd ${SSH_DIRECTORY} && exec \${SHELL} -l"
+			fi
+		else
+			"${TERMINAL?'No terminals available'}" ssh -t "${SSH_USER}@${SSH_HOST}" "cd ${SSH_DIRECTORY} && exec \${SHELL} -l"
+		fi
+		;;
+	*)
+		"${TERMINAL?'No terminals available'}" "${directory}"
+		;;
+esac
