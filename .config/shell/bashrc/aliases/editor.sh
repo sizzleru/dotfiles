1eb8a0bd0f4dce4c8ad7bc4a3d8a3dc544549d6a
sizzleru <github@sizzleru.com>
2026-06-15 10:21:37 +1000

    Generalised editor to also be filebrowser if needed

--- .config/shell/bashrc/aliases/editor.sh
+++ .config/shell/bashrc/aliases/editor.sh
@@ -1,8 +1,15 @@
-function v() {
+v() {
+
 	if [ "${#}" -eq 0 ]; then
-		"${EDITOR}" .
-	else
-		"${EDITOR}" ${@}
+		set -- '.'
+	fi
+
+	local tmp="$( mktemp -t 'file-cwd.XXXXXX' )"
+	FILEBROWSER_LASTDIR_FILE="${tmp}" command "${EDITOR}" ${@}
+
+	if [ -f "${tmp}" ]; then
+		cd "$( cat "${tmp}" )"
+		rm -f "${tmp}"
 	fi
 }
 alias vi='v'
--- .config/shell/bashrc/aliases/main.sh
+++ .config/shell/bashrc/aliases/main.sh
@@ -1,4 +1,4 @@
-for file in editor fetch git trashcli; do
+for file in editor fetch git; do
 	if [ -f "${HOME}/.config/shell/bashrc/aliases/${file}.sh" ]; then
 		. "${HOME}/.config/shell/bashrc/aliases/${file}.sh"
 	fi
